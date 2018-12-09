945961d7645d2bb131c31b141bbeaceb3400381b
Rohan de Jongh <rohandejongh@gmail.com>
2018-12-09 14:47:25 +0200

    Initial commit

---
+++ app/src/main/java/com/roebou/openapitest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.roebou.openapitest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToSignUpAct (View view) {
+        Intent signUp = new Intent(this, SignUpActivity.class) ;
+        startActivity(signUp);
+    }
+}
